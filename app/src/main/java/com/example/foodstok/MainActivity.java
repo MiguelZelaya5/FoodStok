28567e78a48d48cf4756ea49445b5cf3d2e1f158
Miguel <99148258+MiguelZelaya5@users.noreply.github.com>
2023-11-12 18:11:29 -0600

    AVANCE DE LLENADO

--- app/src/main/java/com/example/foodstok/MainActivity.java
+++ app/src/main/java/com/example/foodstok/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,17 +18,29 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.foodstok.inventario.datos;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView info;
+    TableLayout tbArticulo;
     FloatingActionButton fab;
     DrawerLayout drawerLayout;
     ImageView menu;
@@ -47,6 +60,53 @@ public class MainActivity extends AppCompatActivity {
         categoria=findViewById(R.id.categoria);
 
         fab = findViewById(R.id.addProducto);
+        tbArticulo = findViewById(R.id.tablaArticulo);
+        tbArticulo.removeAllViews();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        int idUsuario=1;
+        String url = "http://192.168.1.5/android_mysq/consulta.php/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("Conexion exitosa");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                View registro = LayoutInflater.from(MainActivity.this).inflate(R.layout.table_row, null, false);
+                                TextView colCategoria = registro.findViewById(R.id.colCategoria);
+                                TextView colArticulo = registro.findViewById(R.id.colArticulo);
+                                TextView colfecha = registro.findViewById(R.id.colfecha);
+                                TextView colfechacadu = registro.findViewById(R.id.colfechacadu);
+                                TextView colcantidad = registro.findViewById(R.id.colcantidad);
+                                TextView colfoto = registro.findViewById(R.id.colfoto);
+
+                                colCategoria.setText(jsonObject.getString("categoria"));
+                                colArticulo.setText(jsonObject.getString("nombrearticulo"));
+                                colfecha.setText(jsonObject.getString("fechafabricacion"));
+                                colfechacadu.setText(jsonObject.getString("fechacaducidad"));
+                                colcantidad.setText(jsonObject.getString("cantidad"));
+                                colfoto.setText(jsonObject.getString("foto"));
+                                tbArticulo.addView(registro);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Manejar errores de la solicitud
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
